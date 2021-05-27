428bbbc9d9742e36dfc2deefd7a50602a8fd961a
ppaulocma <ppaulocma@gmail.com>
2021-05-27 14:14:18 -0300

    Chat Controller

--- src/main/java/br/com/codificando/controller/IndexController.java
+++ src/main/java/br/com/codificando/controller/IndexController.java
@@ -2,17 +2,31 @@ package br.com.codificando.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.codificando.model.Usuario;
 import br.com.codificando.repository.UsuarioRepository;
+import br.com.codificando.service.UsuarioService;
 
 @Controller
 public class IndexController {
 	@Autowired
 	UsuarioRepository usuarioRepository;
 	
+	@Autowired
+	UsuarioService usuarioService;
+	
 	@GetMapping("/")
 		public String getIndex(){
 			return "index";
+	}
+	
+	@GetMapping("/chat")
+	public String getChat(Model model){
+		Usuario usuario = usuarioService.usuarioLogado();
+		model.addAttribute("usuario", usuario);
+		return "chat";
 	}
 
 }
